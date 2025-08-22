8f283781bec5cee8b9f0ce78fd1b809e2c7304e0
ken kagai <jibken80@gmail.com>
2025-08-22 23:34:40 +0300

    add  dynamic content generator for the website through javascript file and an add quote form for the user in the html file

--- dom_manipulation/script.js
+++ dom_manipulation/script.js
@@ -1,6 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
 
     const quotes = [];
+    const body = document.querySelector('body');
+    const display = document.querySelector('#quoteDisplay');
     const heading = document.querySelector('#heading');
 
     heading.style.fontSize = '3em';
@@ -13,5 +15,65 @@ document.addEventListener('DOMContentLoaded', () => {
         heading.style.color = 'blue';
     })
 
-     
+     function showRandomQuote() {
+        const randomIndex = Math.floor(Math.random()* quotes.length);
+        const quotes = quotes[randomIndex];
+
+        if (!display) {
+            const display = document.createElement('div');
+            display.id = 'quoteDisplay';
+            body.appendChild(display);
+        }
+
+        const quotePara = document.createElement('p');
+        quotePara.textContent = quotes;
+        const quoteCategory = document.createElement('p');
+        quoteCategory.textContent = `category: ${quotes.category}`;
+
+        display.innerHTML = '';
+        
+        display.appendChild(quotePara);
+        display.appendChild(quoteCategory);
+     }
+
+     function createAddQuoteForm() {
+        const form = document.createElement('form');
+        form.id = 'quoteForm';
+
+        const quoteInput = document.createElement('input');
+        quoteInput.type = 'text';
+        quoteInput.placeholder = 'Enter New Quote';
+        quoteInput.id = 'quoteInput';
+        quoteInput.required = true;
+
+
+        const categoryInput = document.createElement('input');
+        categoryInput.type = 'text';
+        categoryInput.placeholder = 'Enter Quote Category';
+        categoryInput.id = 'categoryInput';
+        categoryInput.required = true;
+
+        const submitBtn = document.createElement('button');
+        submitBtn.type = 'submit';
+        submitBtn.textContent = 'Add Quote';
+
+        form.appendChild(quoteInput);
+        form.appendChild(categoryInput);
+        form.appendChild(submitBtn);
+
+        body.appendChild(form);
+
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+
+            quotes.push({
+                text: quoteInput.value,
+                category: categoryInput.value
+            });
+        })
+        textInput.value = '';
+        categoryInput.value = '';
+
+        
+     }
 })
