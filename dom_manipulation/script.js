0857bc8929b93966c93bf0d437a5476327942b0e
ken kagai <jibken80@gmail.com>
2025-08-23 00:41:41 +0300

    feat: fixed typos in script file and remove the script form created in it

--- dom_manipulation/script.js
+++ dom_manipulation/script.js
@@ -1,81 +1,57 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-    const quotes = [];
+    const quotes = [
+         { text: "Be yourself; everyone else is already taken.", category: "Life" },
+        { text: "Simplicity is the soul of efficiency.", category: "Programming" }
+    ];
     const body = document.querySelector('body');
     const display = document.querySelector('#quoteDisplay');
     const heading = document.querySelector('#heading');
+    const newQuoteButton = document.querySelector('#newQuote');
+    const quoteInput = document.querySelector('#newQuoteText');
+    const categoryInput = document.querySelector('#newQuoteCategory');
 
     heading.style.fontSize = '3em';
     heading.style.fontFamily = "'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif";
     heading.style.margin = '10px';
-    heading.addEventListener('mouseover', () => {
-        heading.style.color = 'red';
-    })
-    heading.addEventListener('mouseout', () => {
-        heading.style.color = 'blue';
-    })
-
-        function showRandomQuote() {
-            const randomIndex = Math.floor(Math.random()* quotes.length);
-            const quotes = quotes[randomIndex];
-
-            if (!display) {
-                const display = document.createElement('div');
-                display.id = 'quoteDisplay';
-                body.appendChild(display);
-            }
-
-            const quotePara = document.createElement('p');
-            quotePara.textContent = quotes;
-            const quoteCategory = document.createElement('p');
-            quoteCategory.textContent = `category: ${quotes.category}`;
-
-            display.innerHTML = '';
+    heading.addEventListener('mouseover', () => heading.style.color = 'red' );
+    heading.addEventListener('mouseout', () => heading.style.color = 'blue' );
+   
+    // Function to display a random quote
+    function showRandomQuote() {
+        const randomIndex = Math.floor(Math.random()* quotes.length);
+        const randomQuote = quotes[randomIndex];
+        
+        if (!display) {
+            const display = document.createElement('div');
+            display.id = 'quoteDisplay';
+            body.appendChild(display);
+        }
+        
+        const quotePara = document.createElement('p');
+        quotePara.textContent = randomQuote.text;
+        const quoteCategory = document.createElement('p');
+        quoteCategory.textContent = randomQuote.category;
+        
+        display.innerHTML = '';
             
-            display.appendChild(quotePara);
-            display.appendChild(quoteCategory);
-        }
-
-        function createAddQuoteForm() {
-            const form = document.createElement('form');
-            form.id = 'quoteForm';
-
-            const quoteInput = document.createElement('input');
-            quoteInput.type = 'text';
-            quoteInput.placeholder = 'Enter New Quote';
-            quoteInput.id = 'quoteInput';
-            quoteInput.required = true;
-
-
-            const categoryInput = document.createElement('input');
-            categoryInput.type = 'text';
-            categoryInput.placeholder = 'Enter Quote Category';
-            categoryInput.id = 'categoryInput';
-            categoryInput.required = true;
-
-            const submitBtn = document.createElement('button');
-            submitBtn.type = 'submit';
-            submitBtn.textContent = 'Add Quote';
-
-            form.appendChild(quoteInput);
-            form.appendChild(categoryInput);
-            form.appendChild(submitBtn);
-
-            body.appendChild(form);
-
-            form.addEventListener('submit', (e) => {
-                e.preventDefault();
-
-                quotes.push({
-                    text: quoteInput.value,
-                    category: categoryInput.value
-                });
-            })
-            textInput.value = '';
+        display.appendChild(quotePara);
+        display.appendChild(quoteCategory);
+    }
+    newQuoteButton.addEventListener('click', showRandomQuote);
+    
+    // Add quotes Dynamically
+    function addQuote() {
+        if (quoteInput.value && categoryInput.value) {
+            
+            quotes.push({
+                text: quoteInput.value,
+                category: categoryInput.value
+            });
+            quoteInput.value = '';
             categoryInput.value = '';
-
-
         }
-        showRandomQuote();
-        createAddQuoteForm();
-})
+    }
+  window.addQuote = addQuote;
+    showRandomQuote();
+});
