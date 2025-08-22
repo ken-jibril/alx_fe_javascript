46811106e859e8bfc4120dc465b3af8e031370de
ken kagai <jibken80@gmail.com>
2025-08-22 23:39:28 +0300

    add empty string function for the inputs

--- dom_manipulation/script.js
+++ dom_manipulation/script.js
@@ -15,67 +15,67 @@ document.addEventListener('DOMContentLoaded', () => {
         heading.style.color = 'blue';
     })
 
-     function showRandomQuote() {
-        const randomIndex = Math.floor(Math.random()* quotes.length);
-        const quotes = quotes[randomIndex];
+        function showRandomQuote() {
+            const randomIndex = Math.floor(Math.random()* quotes.length);
+            const quotes = quotes[randomIndex];
 
-        if (!display) {
-            const display = document.createElement('div');
-            display.id = 'quoteDisplay';
-            body.appendChild(display);
+            if (!display) {
+                const display = document.createElement('div');
+                display.id = 'quoteDisplay';
+                body.appendChild(display);
+            }
+
+            const quotePara = document.createElement('p');
+            quotePara.textContent = quotes;
+            const quoteCategory = document.createElement('p');
+            quoteCategory.textContent = `category: ${quotes.category}`;
+
+            display.innerHTML = '';
+            
+            display.appendChild(quotePara);
+            display.appendChild(quoteCategory);
         }
 
-        const quotePara = document.createElement('p');
-        quotePara.textContent = quotes;
-        const quoteCategory = document.createElement('p');
-        quoteCategory.textContent = `category: ${quotes.category}`;
+        function createAddQuoteForm() {
+            const form = document.createElement('form');
+            form.id = 'quoteForm';
 
-        display.innerHTML = '';
-        
-        display.appendChild(quotePara);
-        display.appendChild(quoteCategory);
-     }
-
-     function createAddQuoteForm() {
-        const form = document.createElement('form');
-        form.id = 'quoteForm';
-
-        const quoteInput = document.createElement('input');
-        quoteInput.type = 'text';
-        quoteInput.placeholder = 'Enter New Quote';
-        quoteInput.id = 'quoteInput';
-        quoteInput.required = true;
+            const quoteInput = document.createElement('input');
+            quoteInput.type = 'text';
+            quoteInput.placeholder = 'Enter New Quote';
+            quoteInput.id = 'quoteInput';
+            quoteInput.required = true;
 
 
-        const categoryInput = document.createElement('input');
-        categoryInput.type = 'text';
-        categoryInput.placeholder = 'Enter Quote Category';
-        categoryInput.id = 'categoryInput';
-        categoryInput.required = true;
+            const categoryInput = document.createElement('input');
+            categoryInput.type = 'text';
+            categoryInput.placeholder = 'Enter Quote Category';
+            categoryInput.id = 'categoryInput';
+            categoryInput.required = true;
 
-        const submitBtn = document.createElement('button');
-        submitBtn.type = 'submit';
-        submitBtn.textContent = 'Add Quote';
+            const submitBtn = document.createElement('button');
+            submitBtn.type = 'submit';
+            submitBtn.textContent = 'Add Quote';
 
-        form.appendChild(quoteInput);
-        form.appendChild(categoryInput);
-        form.appendChild(submitBtn);
+            form.appendChild(quoteInput);
+            form.appendChild(categoryInput);
+            form.appendChild(submitBtn);
 
-        body.appendChild(form);
+            body.appendChild(form);
 
-        form.addEventListener('submit', (e) => {
-            e.preventDefault();
+            form.addEventListener('submit', (e) => {
+                e.preventDefault();
 
-            quotes.push({
-                text: quoteInput.value,
-                category: categoryInput.value
-            });
-        })
-        textInput.value = '';
-        categoryInput.value = '';
+                quotes.push({
+                    text: quoteInput.value,
+                    category: categoryInput.value
+                });
+            })
+            textInput.value = '';
+            categoryInput.value = '';
 
 
-     }
-     showRandomQuote();
-     createAddQuoteForm();
+        }
+        showRandomQuote();
+        createAddQuoteForm();
 })
